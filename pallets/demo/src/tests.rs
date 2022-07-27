0504551e04d723da16ebd71d992053e662f99bed
NhanVH_Fabbi <nhanvh@fabbi.com.vn>
2022-07-27 13:59:48 +0700

    Unittest for pallet-demo

--- pallets/demo/src/tests.rs
+++ pallets/demo/src/tests.rs
@@ -5,9 +5,9 @@ use frame_support::{assert_noop, assert_ok};
 fn it_works_for_default_value() {
 	new_test_ext().execute_with(|| {
 		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
+		assert_ok!(DemoModule::create_student(Origin::signed(1), b"nhanvh_fabbi".to_vec(), 28));
 		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
+		assert_eq!(DemoModule::student_id(), 1);
 	});
 }
 
@@ -15,6 +15,6 @@ fn it_works_for_default_value() {
 fn correct_error_for_none_value() {
 	new_test_ext().execute_with(|| {
 		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
+		assert_noop!(DemoModule::create_student(Origin::signed(1),  b"nhanvh_fabbi".to_vec(), 18), Error::<Test>::TooYoung);
 	});
 }
